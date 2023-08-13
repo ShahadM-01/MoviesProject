5bb55e626fecb08d06975cf58b9256e7a5f0bccb
ShahadM-01 <139268398+ShahadM-01@users.noreply.github.com>
2023-08-14 00:55:46 +0300

    Create the necessary components

---
+++ src/app/watched-list/watched-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watched-list',
+  templateUrl: './watched-list.component.html',
+  styleUrls: ['./watched-list.component.css']
+})
+export class WatchedListComponent {
+
+}
