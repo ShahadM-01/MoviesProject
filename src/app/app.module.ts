f141ce58bd47974f7978d12ff09d5315cd5e3f16
ShahadM-01 <139268398+ShahadM-01@users.noreply.github.com>
2023-08-13 22:27:49 +0300

    Upload new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieItemComponent } from './movie-item/movie-item.component';
+import { MovieFormComponent } from './movie-form/movie-form.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MovieItemComponent,
+    MovieFormComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
