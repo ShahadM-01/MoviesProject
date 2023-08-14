7eaea01b43aaa2795ae4445dcea3c0abf2653b63
ShahadM-01 <139268398+ShahadM-01@users.noreply.github.com>
2023-08-15 01:19:15 +0300

    create remove from watchlist method

--- src/app/watchlist/watchlist.service.ts
+++ src/app/watchlist/watchlist.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class WatchlistService {
-  watchlist: string[] = [];
+    watchlist: string[] = [];
 
-  addToWatchlist(movie: string) {
-    if (!this.watchlist.includes(movie)) {
-      this.watchlist.push(movie);
+    addToWatchlist(movie: string) {
+        if (!this.watchlist.includes(movie)) {
+            this.watchlist.push(movie);
+        }
     }
-  }
+    removeFromWatchlist(movie: string) {
+        const index = this.watchlist.indexOf(movie);
+        if (index !== -1) {
+            this.watchlist.splice(index, 1);
+        }
+    }
 
 
 }
